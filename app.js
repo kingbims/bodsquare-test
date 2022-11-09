994e0434d98370eca1c28f0916b0af5723dd9ef1
Kingbims <moremiabimbolamorakinyo@gmail.com>
2022-11-09 15:15:13 +0100

    app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+const app = require('./src/startup/routes')
+const http = require('http').createServer(app)
+const db = require('./src/startup/db')
+const port = process.env.port || 3000
+
+http.listen(port, () => {
+    console.log(`server starting on port: ${port}`)
+})
